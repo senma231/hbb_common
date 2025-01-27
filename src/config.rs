6edbdc091e392beff4d65bef8601fb14383bdadc
senma231 <senmago231@gmail.com>
2025-01-27 12:06:21 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,9 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["6536c14c-0be5-4f0b-bd4c-ab685c4eafc9.909981.xyz"];
+pub const API_SERVER: &[&str] = &["http://6536c14c-0be5-4f0b-bd4c-ab685c4eafc9.909981.xyz:21114"];
+pub const PUBLIC_RS_PUB_KEY: &str = "zHCCLZRJ7a36QlXmj6kVqRznfYt+UCCLd4URH4nytjg=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
